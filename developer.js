caf559544f83520e4dfd1d86070e210f6a95dc98
mpn01 <mnowinski01@gmail.com>
2020-01-24 15:12:46 +0100

    Another test commit

--- developer.js
+++ developer.js
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 document.write(Hey)
+=======
+var proffesion;
+proffesion = ['frontend','java'];
+document.getElementsByClassName('container');
+document.write(proffesion[1]);
+>>>>>>> Add initial files
