a82a23502aa20f74fd82ad9cc157fcb30cf8ed2d
Marcin <mnowinski01@protonmail.com>
2020-07-05 21:53:09 +0200

    finished developer script, small fixes in code

--- developer.js
+++ developer.js
@@ -1,8 +1,10 @@
-<<<<<<< HEAD
-document.write(Hey)
-=======
-var proffesion;
-proffesion = ['frontend','java'];
-document.getElementsByClassName('container');
-document.write(proffesion[1]);
->>>>>>> Add initial files
+const proffesionTable = ['frontend','java'];
+var proffesionName = document.getElementById('proffesion-name');
+proffesionName.innerHTML = proffesionTable[0];
+var random = Math.random();
+
+if(random<0.5){
+    proffesionName.innerHTML = proffesionTable[0];
+} else {
+    proffesionName.innerHTML = proffesionTable[1];
+}
