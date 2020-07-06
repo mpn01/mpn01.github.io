36cbc5fa3a8cc00ecd9edd93538972166cd88ebd
Marcin <mnowinski01@protonmail.com>
2020-07-06 10:03:44 +0200

    adding go and backend developer to js table

--- developer.js
+++ developer.js
@@ -1,10 +1,14 @@
-const proffesionTable = ['frontend','java'];
+const proffesionTable = ['frontend','java','go','backend'];
 var proffesionName = document.getElementById('proffesion-name');
 proffesionName.innerHTML = proffesionTable[0];
 var random = Math.random();
 
-if(random<0.5){
+if(0.25>random>0){
     proffesionName.innerHTML = proffesionTable[0];
-} else {
+} else if(0.50>random>0.25) {
     proffesionName.innerHTML = proffesionTable[1];
+} else if(0.75>random>0.50) {
+    proffesionName.innerHTML = proffesionTable[2];
+} else {
+    proffesionName.innerHTML = proffesionTable[3];
 }
