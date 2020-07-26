52d6cf0fb23bba1f82df1e41cc84e0c31da0189a
Marcin <mnowinski01@protonmail.com>
2020-07-26 20:03:17 +0200

    changes to contact and main buttons

--- developer.js
+++ developer.js
@@ -4,7 +4,28 @@ proffesionName.innerHTML = proffesionTable[0];
 var random = Math.random();
 var email = document.querySelector('.form-email-copy');
 email.addEventListener('click', copyToClipboard);
+var contactBtn = document.querySelector('.contact-btn');
+var contact = document.getElementById('contact');
+var contactCloseBtn = document.getElementById('contact-close-btn');
+var btnStatus = false;
 
+function showContact() {
+    if(btnStatus == false){
+        contact.style.display = 'flex';
+        btnStatus = true;
+        contact.style.animation = 'slideIn .5s';
+    }
+}
+function hideContact() {
+    if(btnStatus == true){
+        contact.style.display = 'none';
+        btnStatus = false;
+        contact.style.animation = 'slideOut 1s';
+    }
+}
+
+contactBtn.addEventListener('click', showContact);
+contactCloseBtn.addEventListener('click', hideContact);
 
 if(0.25>random>0){
     proffesionName.innerHTML = proffesionTable[0];
